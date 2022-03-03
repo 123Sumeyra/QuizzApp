5a814f7cf10410123036ec807ecf2f5e3151bdd5
123Sumeyra <ozugursumeyra@gmail.com>
2022-03-04 00:20:27 +0300

    add some functions

--- QuizApp/app/src/main/java/com/sum/quizapp/QuizQuestionActivity.kt
+++ QuizApp/app/src/main/java/com/sum/quizapp/QuizQuestionActivity.kt
@@ -1,11 +1,23 @@
 package com.sum.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.sum.quizapp.databinding.ActivityQuizQuestionBinding
+import org.w3c.dom.Text
 
-class QuizQuestionActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var currentPosition:Int=1
+    private var questionList:ArrayList<Question> ? = null
+    private var selecedOption:Int=0
+
+
     private lateinit var binding:ActivityQuizQuestionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,11 +26,23 @@ class QuizQuestionActivity : AppCompatActivity() {
         setContentView(view)
 
 
-        val questionList = Constant.getQuestion()
-        Log.i("Question Size:", "${questionList.size}")
+        questionList = Constant.getQuestion()
+       // Log.i("Question Size:", "${questionList.size}")
+        setQuestion()
 
-        val  currentPosition =1
-        val question:Question ?= questionList[currentPosition-1]
+        binding.optionOne.setOnClickListener(this)
+        binding.optionTwo.setOnClickListener(this)
+        binding.optionThree.setOnClickListener(this)
+        binding.optionFour.setOnClickListener(this)
+
+
+
+    }
+
+    private fun setQuestion(){
+        currentPosition =1
+        val question =questionList!![currentPosition-1]
+        defaultOptionsView()
 
         binding.progressBar.progress =currentPosition
         binding.tvProgress.text = "$currentPosition" + "/" + binding.progressBar.max
@@ -33,11 +57,27 @@ class QuizQuestionActivity : AppCompatActivity() {
 
 
 
+    }
 
 
+    fun defaultOptionsView(){
+        val options= ArrayList<TextView> ()
+        options.add(0,binding.optionOne)
+        options.add(1,binding.optionTwo)
+        options.add(2,binding.optionThree)
+        options.add(3,binding.optionFour)
 
+        for(option in options)
+        {
+            option.setTextColor(Color.parseColor("#555151"))
+            option.typeface= Typeface.DEFAULT
+            option.background= ContextCompat.getDrawable(
+                this,R.drawable.defoult_option_one_border)
 
+        }
+    }
 
-
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
