7040014383a176770645965b84681a5a4287feb4
123Sumeyra <ozugursumeyra@gmail.com>
2022-03-03 17:59:39 +0300

    first screen

--- QuizApp/app/src/main/java/com/sum/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/sum/quizapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.sum.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
